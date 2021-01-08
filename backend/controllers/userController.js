9198ef1adb1a7acb185feebfba4926ef9d9e5d26
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-01-08 19:34:13 +0100

    end of section7:backend authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,24 +24,55 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc     Register a new user
+//@route     POST /api/users
+//@acces     Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.send(400);
+    throw new Error("User already exists.");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.send(201).json({
+      //201 = something is created
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 //@desc      Get user profile
 //@route     GET /api/users/profile
 //@acces     Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
   const user = await User.findById(req.user._id);
-  //   if (user) {
-  //     res.json({
-  //       _id: user._id,
-  //       name: user.name,
-  //       email: user.email,
-  //       isAdmin: user.isAdmin,
-  //     });
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("User not found");
-  //   }
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found..");
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
